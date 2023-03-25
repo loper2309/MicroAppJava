28d195d951a50eb4add6e2586811393ad411f76c
loper2309 <116813364+loper2309@users.noreply.github.com>
2023-03-25 12:21:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,19 @@ window.onload = () => {
                 latitude: e.detail.position.latitude + 0.001,
                 longitude: e.detail.position.longitude
             });
+            const entity1 = document.createElement("a-box");
+            entity.setAttribute("scale", {
+                x: 20, 
+                y: 20,
+                z: 20
+            });
+            entity.setAttribute('material', { color: 'green' } );
+            entity.setAttribute('gps-new-entity-place', {
+                latitude: e.detail.position.latitude ,
+                longitude: e.detail.position.longitude + 0.001
+            });
             document.querySelector("a-scene").appendChild(entity);
+            document.querySelector("a-scene").appendChild(entity1);
         }
         testEntityAdded = true;
     });
